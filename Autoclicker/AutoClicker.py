458da2da9dda156f2bd33a46629c646488180dd8
tom7220 <tom7220@users.noreply.github.com>
2022-05-01 15:21:37 +1000

    Update AutoClicker.py

--- Autoclicker/AutoClicker.py
+++ Autoclicker/AutoClicker.py
@@ -1,8 +1,6 @@
 import keyboard
 import pyautogui
-import sys
 while(True):
             pyautogui.click()
             if keyboard.is_pressed('q'):
                 quit();
-            print("Fail safe Trigged")
