a3cf7125bd4a55ab7fc192956ac43da723ce48f6
tom7220 <tom7220@users.noreply.github.com>
2022-05-01 15:23:23 +1000

    Update AutoClicker.py

--- Autoclicker/AutoClicker.py
+++ Autoclicker/AutoClicker.py
@@ -1,5 +1,7 @@
 import keyboard
 import pyautogui
+# if the button q is pressed it will prompt asking you if you want to quit.
+# the button q is the fail safe for this project you can also move the cursor to the corners as another failsafe option.
 while(True):
             pyautogui.click()
             if keyboard.is_pressed('q'):
